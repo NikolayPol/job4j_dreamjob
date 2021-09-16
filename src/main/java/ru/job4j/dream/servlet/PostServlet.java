2a0d024ab9651f551141e4ac783f6fb36097c84e
NikolayPolegaev <N5445@yandex.ru>
2021-09-16 05:46:28 +0300

    1. JSTL замена Scriplets [#2516]

--- src/main/java/ru/job4j/dream/servlet/PostServlet.java
+++ src/main/java/ru/job4j/dream/servlet/PostServlet.java
@@ -17,14 +17,15 @@ import java.io.IOException;
  */
 public class PostServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)throws ServletException,
+            IOException {
         req.setAttribute("posts", Store.instOf().findAllPosts());
         req.getRequestDispatcher("/post/posts.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
+            throws IOException {
         req.setCharacterEncoding("UTF-8");
         Store.instOf().save(new Post(Integer.parseInt(req.getParameter("id")),
                 req.getParameter("name")));
