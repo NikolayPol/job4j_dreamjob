dcacc3fd8824bc45ee6015f55b4e949e1003bd46
NikolayPolegaev <N5445@yandex.ru>
2021-09-23 02:16:10 +0300

    23.09.21 2:15

--- src/test/java/ru/job4j/dream/servlet/CandidateServletTest.java
+++ src/test/java/ru/job4j/dream/servlet/CandidateServletTest.java
@@ -1,77 +1,77 @@
-package ru.job4j.dream.servlet;
-
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
-import ru.job4j.dream.model.Candidate;
-import ru.job4j.dream.store.MemStore;
-import ru.job4j.dream.store.PsqlStore;
-import ru.job4j.dream.store.Store;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
-
-import static org.mockito.Mockito.*;
-import static org.powermock.api.mockito.PowerMockito.when;
-
-/**
- * Класс CandidateServletTest тестирует класс CandidateServlet
- * с использованием библиотеки Mockito.
- *
- * @author Nikolay Polegaev
- * @version 1.0 22.09.2021
- */
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(PsqlStore.class)
-public class CandidateServletTest {
-    @Test
-    public void whenDoGetThenForwardToJsp() throws ServletException, IOException {
-        Store store = MemStore.instOf();
-        String path = "/candidate/candidates.jsp";
-
-        PowerMockito.mockStatic(PsqlStore.class);
-        when(PsqlStore.instOf()).thenReturn(store);
-
-        HttpServletRequest req = mock(HttpServletRequest.class);
-        HttpServletResponse resp = mock(HttpServletResponse.class);
-        RequestDispatcher dispatcher = mock(RequestDispatcher.class);
-        HttpSession session = mock(HttpSession.class);
-
-        when(req.getSession()).thenReturn(session);
-        when(req.getRequestDispatcher(path)).thenReturn(dispatcher);
-
-        new CandidateServlet().doGet(req, resp);
-
-        verify(req, times(1)).getRequestDispatcher(path);
-        verify(req, times(1)).getSession();
-        verify(dispatcher).forward(req, resp);
-    }
-
-    @Test
-    public void whenDoPostThenCreatePost() throws IOException {
-        Store store = MemStore.instOf();
-
-        PowerMockito.mockStatic(PsqlStore.class);
-        when(PsqlStore.instOf()).thenReturn(store);
-
-        HttpServletRequest req = mock(HttpServletRequest.class);
-        HttpServletResponse resp = mock(HttpServletResponse.class);
-
-        when(req.getParameter("id")).thenReturn("4");
-        when(req.getParameter("name")).thenReturn("SuperJunior Java");
-
-        new CandidateServlet().doPost(req, resp);
-
-        Candidate result = (Candidate) store.findAllCandidates().toArray()[3];
-        Assert.assertEquals(result.getName(), "SuperJunior Java");
-        verify(resp).sendRedirect(req.getContextPath() + "/candidates.do");
-
-    }
-}
+//package ru.job4j.dream.servlet;
+//
+//import org.junit.Assert;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.powermock.api.mockito.PowerMockito;
+//import org.powermock.core.classloader.annotations.PrepareForTest;
+//import org.powermock.modules.junit4.PowerMockRunner;
+//import ru.job4j.dream.model.Candidate;
+//import ru.job4j.dream.store.MemStore;
+//import ru.job4j.dream.store.PsqlStore;
+//import ru.job4j.dream.store.Store;
+//
+//import javax.servlet.RequestDispatcher;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpSession;
+//import java.io.IOException;
+//
+//import static org.mockito.Mockito.*;
+//import static org.powermock.api.mockito.PowerMockito.when;
+//
+///**
+// * Класс CandidateServletTest тестирует класс CandidateServlet
+// * с использованием библиотеки Mockito.
+// *
+// * @author Nikolay Polegaev
+// * @version 1.0 22.09.2021
+// */
+//@RunWith(PowerMockRunner.class)
+//@PrepareForTest(PsqlStore.class)
+//public class CandidateServletTest {
+//    @Test
+//    public void whenDoGetThenForwardToJsp() throws ServletException, IOException {
+//        Store store = MemStore.instOf();
+//        String path = "/candidate/candidates.jsp";
+//
+//        PowerMockito.mockStatic(PsqlStore.class);
+//        when(PsqlStore.instOf()).thenReturn(store);
+//
+//        HttpServletRequest req = mock(HttpServletRequest.class);
+//        HttpServletResponse resp = mock(HttpServletResponse.class);
+//        RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+//        HttpSession session = mock(HttpSession.class);
+//
+//        when(req.getSession()).thenReturn(session);
+//        when(req.getRequestDispatcher(path)).thenReturn(dispatcher);
+//
+//        new CandidateServlet().doGet(req, resp);
+//
+//        verify(req, times(1)).getRequestDispatcher(path);
+//        verify(req, times(1)).getSession();
+//        verify(dispatcher).forward(req, resp);
+//    }
+//
+//    @Test
+//    public void whenDoPostThenCreatePost() throws IOException {
+//        Store store = MemStore.instOf();
+//
+//        PowerMockito.mockStatic(PsqlStore.class);
+//        when(PsqlStore.instOf()).thenReturn(store);
+//
+//        HttpServletRequest req = mock(HttpServletRequest.class);
+//        HttpServletResponse resp = mock(HttpServletResponse.class);
+//
+//        when(req.getParameter("id")).thenReturn("4");
+//        when(req.getParameter("name")).thenReturn("SuperJunior Java");
+//
+//        new CandidateServlet().doPost(req, resp);
+//
+//        Candidate result = (Candidate) store.findAllCandidates().toArray()[3];
+//        Assert.assertEquals(result.getName(), "SuperJunior Java");
+//        verify(resp).sendRedirect(req.getContextPath() + "/candidates.do");
+//
+//    }
+//}
--- src/test/java/ru/job4j/dream/servlet/PostServletTest.java
+++ src/test/java/ru/job4j/dream/servlet/PostServletTest.java
@@ -1,113 +1,113 @@
-package ru.job4j.dream.servlet;
-
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
-import ru.job4j.dream.model.Post;
-import ru.job4j.dream.store.MemStore;
-import ru.job4j.dream.store.PsqlStore;
-import ru.job4j.dream.store.Store;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
-
-import static org.mockito.Mockito.*;
-import static org.powermock.api.mockito.PowerMockito.when;
-
-/**
- * Класс PostServletTest тестирует класс PostServlet
- * с использованием библиотеки Mockito.
- * 1) Чтобы запуск теста производился за счет библиотеки PowerMock,
- * необходимо указать аннотации ниже:
- * RunWith(PowerMockRunner.class)
- * PrepareForTest(PsqlStore.class)
- * 2) Сначала мы создаем хранилище для работы с памятью:
- * Store<Post> store = MemStore.instOf();
- * 3) Далее мы "глушим" статику. Первый метод позволяет создать объект,
- * не подтягивания статические зависимости. Второй указывает, что при вызове
- * статического метода для получения хранилища нужно вернуть хранилище MemStore:
- * PowerMockito.mockStatic(PsqlStore.class);
- * PowerMockito.when(PsqlStore.getStore()).thenReturn(store);
- * 4) Создаем заглушки для ответа и запроса:
- * HttpServletRequest req = mock(HttpServletRequest.class);
- * HttpServletResponse resp = mock(HttpServletResponse.class);
- * 5) Указываем, что возвращать при вызове методов заглушки запроса:
- * PowerMockito.when(req.getParameter("id")).thenReturn("0");
- * PowerMockito.when(req.getParameter("name")).thenReturn("n");
- * PowerMockito.when(req.getParameter("description")).thenReturn("d");
- * 6) Выполняем запрос:
- * new PostServlet().doPost(req, resp);
- * 7) Проверяем результат:
- * Post result = store.findAll().iterator().next();
- * Assert.assertThat(result.getName(), Is.is("n"));
- * Assert.assertThat(result.getDescription(), Is.is("d"));
- *
- * verify-проверка.
- * verify(req, times(1)).getRequestDispatcher(path); - проверяет, что вызов 1 раз request установит
- * у объекта RequestDispatcher параметр path - "/post/posts.jsp".
- *
- * verify(req, times(1)).getSession(); - проверка, что вызывается объект сессии 1 раз.
- *
- * verify(dispatcher).forward(req, resp); - проверка, что
- * у dispatcher вызван метод forward с объектами req и resp.
- *
- * verify(resp).sendRedirect(req.getContextPath() + "/posts.do"); - проверка, что
- * redirect вызывается с заданным адресом.
- *
- * @version 1.0 22.09.2021
- * @author Nikolay Polegaev
- */
-
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(PsqlStore.class)
-public class PostServletTest {
-    @Test
-    public void whenDoGetThenForwardToJsp() throws ServletException, IOException {
-        Store store = MemStore.instOf();
-        String path = "/post/posts.jsp";
-
-        PowerMockito.mockStatic(PsqlStore.class);
-        when(PsqlStore.instOf()).thenReturn(store);
-
-        HttpServletRequest req = mock(HttpServletRequest.class);
-        HttpServletResponse resp = mock(HttpServletResponse.class);
-        RequestDispatcher dispatcher = mock(RequestDispatcher.class);
-        HttpSession session = mock(HttpSession.class);
-
-        when(req.getSession()).thenReturn(session);
-        when(req.getRequestDispatcher(path)).thenReturn(dispatcher);
-
-        new PostServlet().doGet(req, resp);
-
-        verify(req, times(1)).getRequestDispatcher(path);
-        verify(req, times(1)).getSession();
-        verify(dispatcher).forward(req, resp);
-    }
-
-    @Test
-    public void whenDoPostThenCreatePost() throws IOException {
-        Store store = MemStore.instOf();
-
-        PowerMockito.mockStatic(PsqlStore.class);
-        when(PsqlStore.instOf()).thenReturn(store);
-
-        HttpServletRequest req = mock(HttpServletRequest.class);
-        HttpServletResponse resp = mock(HttpServletResponse.class);
-
-        when(req.getParameter("id")).thenReturn("4");
-        when(req.getParameter("name")).thenReturn("SuperJunior Java Job");
-
-        new PostServlet().doPost(req, resp);
-
-        Post result = (Post) store.findAllPosts().toArray()[3];
-        Assert.assertEquals(result.getName(), "SuperJunior Java Job");
-        verify(resp).sendRedirect(req.getContextPath() + "/posts.do");
-    }
-}
+//package ru.job4j.dream.servlet;
+//
+//import org.junit.Assert;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.powermock.api.mockito.PowerMockito;
+//import org.powermock.core.classloader.annotations.PrepareForTest;
+//import org.powermock.modules.junit4.PowerMockRunner;
+//import ru.job4j.dream.model.Post;
+//import ru.job4j.dream.store.MemStore;
+//import ru.job4j.dream.store.PsqlStore;
+//import ru.job4j.dream.store.Store;
+//
+//import javax.servlet.RequestDispatcher;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpSession;
+//import java.io.IOException;
+//
+//import static org.mockito.Mockito.*;
+//import static org.powermock.api.mockito.PowerMockito.when;
+//
+///**
+// * Класс PostServletTest тестирует класс PostServlet
+// * с использованием библиотеки Mockito.
+// * 1) Чтобы запуск теста производился за счет библиотеки PowerMock,
+// * необходимо указать аннотации ниже:
+// * RunWith(PowerMockRunner.class)
+// * PrepareForTest(PsqlStore.class)
+// * 2) Сначала мы создаем хранилище для работы с памятью:
+// * Store<Post> store = MemStore.instOf();
+// * 3) Далее мы "глушим" статику. Первый метод позволяет создать объект,
+// * не подтягивания статические зависимости. Второй указывает, что при вызове
+// * статического метода для получения хранилища нужно вернуть хранилище MemStore:
+// * PowerMockito.mockStatic(PsqlStore.class);
+// * PowerMockito.when(PsqlStore.getStore()).thenReturn(store);
+// * 4) Создаем заглушки для ответа и запроса:
+// * HttpServletRequest req = mock(HttpServletRequest.class);
+// * HttpServletResponse resp = mock(HttpServletResponse.class);
+// * 5) Указываем, что возвращать при вызове методов заглушки запроса:
+// * PowerMockito.when(req.getParameter("id")).thenReturn("0");
+// * PowerMockito.when(req.getParameter("name")).thenReturn("n");
+// * PowerMockito.when(req.getParameter("description")).thenReturn("d");
+// * 6) Выполняем запрос:
+// * new PostServlet().doPost(req, resp);
+// * 7) Проверяем результат:
+// * Post result = store.findAll().iterator().next();
+// * Assert.assertThat(result.getName(), Is.is("n"));
+// * Assert.assertThat(result.getDescription(), Is.is("d"));
+// *
+// * verify-проверка.
+// * verify(req, times(1)).getRequestDispatcher(path); - проверяет, что вызов 1 раз request установит
+// * у объекта RequestDispatcher параметр path - "/post/posts.jsp".
+// *
+// * verify(req, times(1)).getSession(); - проверка, что вызывается объект сессии 1 раз.
+// *
+// * verify(dispatcher).forward(req, resp); - проверка, что
+// * у dispatcher вызван метод forward с объектами req и resp.
+// *
+// * verify(resp).sendRedirect(req.getContextPath() + "/posts.do"); - проверка, что
+// * redirect вызывается с заданным адресом.
+// *
+// * @version 1.0 22.09.2021
+// * @author Nikolay Polegaev
+// */
+//
+//@RunWith(PowerMockRunner.class)
+//@PrepareForTest(PsqlStore.class)
+//public class PostServletTest {
+//    @Test
+//    public void whenDoGetThenForwardToJsp() throws ServletException, IOException {
+//        Store store = MemStore.instOf();
+//        String path = "/post/posts.jsp";
+//
+//        PowerMockito.mockStatic(PsqlStore.class);
+//        when(PsqlStore.instOf()).thenReturn(store);
+//
+//        HttpServletRequest req = mock(HttpServletRequest.class);
+//        HttpServletResponse resp = mock(HttpServletResponse.class);
+//        RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+//        HttpSession session = mock(HttpSession.class);
+//
+//        when(req.getSession()).thenReturn(session);
+//        when(req.getRequestDispatcher(path)).thenReturn(dispatcher);
+//
+//        new PostServlet().doGet(req, resp);
+//
+//        verify(req, times(1)).getRequestDispatcher(path);
+//        verify(req, times(1)).getSession();
+//        verify(dispatcher).forward(req, resp);
+//    }
+//
+//    @Test
+//    public void whenDoPostThenCreatePost() throws IOException {
+//        Store store = MemStore.instOf();
+//
+//        PowerMockito.mockStatic(PsqlStore.class);
+//        when(PsqlStore.instOf()).thenReturn(store);
+//
+//        HttpServletRequest req = mock(HttpServletRequest.class);
+//        HttpServletResponse resp = mock(HttpServletResponse.class);
+//
+//        when(req.getParameter("id")).thenReturn("4");
+//        when(req.getParameter("name")).thenReturn("SuperJunior Java Job");
+//
+//        new PostServlet().doPost(req, resp);
+//
+//        Post result = (Post) store.findAllPosts().toArray()[3];
+//        Assert.assertEquals(result.getName(), "SuperJunior Java Job");
+//        verify(resp).sendRedirect(req.getContextPath() + "/posts.do");
+//    }
+//}
